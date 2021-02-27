edd2fbcc6f2507537ebba22bd2a3e76a4ff9e0a3
jonathantvrs <jonathantvrs@ufrn.edu.br>
2021-02-27 16:17:10 -0300

    implementaçao de modelos e migraçoes de category e transaction

--- src/models/Transaction.ts
+++ src/models/Transaction.ts
@@ -1,16 +1,42 @@
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+  OneToOne,
+  JoinColumn,
+} from 'typeorm';
+import Category from './Category';
+
+@Entity('transactions')
 class Transaction {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @Column()
   title: string;
 
+  @Column({
+    type: 'enum',
+    enum: ['income', 'outcome'],
+  })
   type: 'income' | 'outcome';
 
+  @Column()
   value: number;
 
+  @Column()
   category_id: string;
 
+  @OneToOne(() => Category)
+  @JoinColumn({ name: 'category_id' })
+  category: Category;
+
+  @CreateDateColumn()
   created_at: Date;
 
+  @UpdateDateColumn()
   updated_at: Date;
 }
 
